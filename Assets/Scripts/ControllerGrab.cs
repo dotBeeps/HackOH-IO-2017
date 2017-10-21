7086d3058a89930b428bc743c56001ccaa727b54
David Grougan <ddogclaw@gmail.com>
2017-10-21 10:16:23 -0400

    Initial Commit

---
+++ Assets/Scripts/ControllerGrab.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ControllerGrab : MonoBehaviour {
+
+    private SteamVR_TrackedController controller;
+    private List<Collider> collisions = new List<Collider>();
+    private GameObject holding;
+
+    private void OnEnable()
+    {
+        controller = transform.parent.GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += HandleTriggerClick;
+        controller.TriggerUnclicked += HandleTriggerUnclick;
+    }
+
+    // For when you're really feeling triggered
+    private void HandleTriggerClick(object sender, ClickedEventArgs e)
+    {
+        Collider toPickUp = collisions.First(col => col.gameObject.tag == Tag.GRABABLE);
+        if (toPickUp != null)
+        {
+            // Pick up behaviour
+            toPickUp.GetComponent<Rigidbody>().isKinematic = true;
+            toPickUp.transform.parent = controller.transform;
+
+        } else
+        {
+            // Other trigger behaviour
+            // IM TRIGERRRRRRRRRRRRRED
+        }
+    }
+
+    private void HandleTriggerUnclick(object sender, ClickedEventArgs e)
+    {
+        if (holding != null)
+        {
+            holding.GetComponent<Rigidbody>().isKinematic = false;
+            holding.transform.parent = controller.transform.parent.parent;
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == Tag.GRABABLE)
+        {
+            SteamVR_Controller.Input((int)controller.controllerIndex).TriggerHapticPulse();
+        }
+        col.SendMessage("OnControllerStartTouch", transform, SendMessageOptions.DontRequireReceiver);
+        collisions.Add(col);
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        col.SendMessage("OnControllerEndTouch", transform, SendMessageOptions.DontRequireReceiver);
+        collisions.Remove(col);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GrabableBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabableBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnControllerStartTouch(Transform controller)
+    {
+
+    }
+
+    void OnControllerEndTouch(Transform controller)
+    {
+
+    }
+}
